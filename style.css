0b8429af5adbd2ac6cfef9ebd7ea767eedd54616
MuradyanElen7 <142836687+MuradyanElen7@users.noreply.github.com>
2023-08-21 22:47:15 +0300

    Update style.css

---
+++ style.css
@@ -0,0 +1,336 @@
+*{
+             margin:0;
+             }
+              a{
+             text-decoration:none;
+             }
+                header{
+                 display:flex;
+                 direction:row;
+                 justify-content:center;
+                 align-items:center;
+                background-image: url(https://i.pinimg.com/originals/8e/46/15/8e46150f790fbefe438d9c2767c32ad1.gif);
+                 height:300px;
+                 overflow:hidden;
+                 position:relative;
+                font-family:fantasy;
+                background-repeat: repeat-x;
+                }
+              .text1{ 
+             font-size:80px;
+             text-align:center;
+             position:absolute;
+                 color:#F11A7B;
+                 padding:20px;
+             }
+              nav {
+                  top:0;
+	
+display:flex;
+                  direction:row;
+	position: relative;
+	width:100%;
+	height: 50px;
+	background:linear-gradient(90deg,#000000,#4E4FEB);
+	
+	font-size: 0;
+}
+nav a {
+    width:100%;
+	line-height: 50px;
+	height: 100%;
+	font-size: 15px;
+	display: inline-block;
+	position: relative;
+	z-index: 1;
+	text-decoration: none;
+	text-transform: uppercase;
+	text-align: center;
+	color: white;
+	cursor: pointer;
+}
+nav .animation {
+    width: 100%;
+	position: absolute;
+	height: 100%;
+	top: 0;
+	z-index: 0;
+	transition: all .5s ease 0s;
+	border-radius: 8px;
+  background-color: transparent;
+}
+#navsolar:hover~.animation {
+	width:20%;
+	left: 0;
+	background-color: #0D1282;
+}
+#navstars:hover~.animation {
+	width: 20%;
+	left: 20%;
+	background-color: #7A0BC0;
+}
+#navasteroids:hover~.animation {
+	width: 20%;
+	left: 40%;
+	background-color: #3498db;
+}
+#navmeteors:hover~.animation {
+	width: 20%;
+	left:60%;
+	background-color: #9b59b6;
+}
+#navcomets:hover~.animation {
+	width: 20%;
+	left: 80%;
+	background-color: #C400C6;
+}
+            main{
+                
+                 width:100%;
+                 float:left;
+  background-repeat:repeat;
+             }
+             #universe{
+                 height:50px;
+                 margin:0;
+             background:linear-gradient(90deg,#0D1282,#4E4FEB);
+             text-align:center;
+                 font-family:serif;
+                 color:#E8FFCE;
+             }
+             .universetext{
+                  background:linear-gradient(90deg,#0D1282,#4E4FEB);
+                 text-indent:15px;
+             font-size:20px;
+                 color:#E8FFCE;
+                 font-family:arial;
+             }
+             #universeimg1{
+               display:flex;
+               align-content:center;
+               justify-content:center;
+                 background-repeat:no-repeat;
+                               width:330px;
+                                           height:280px;
+                                margin:5px;
+                                overflow:hidden;
+                 float:left;
+                 border-radius:20px;
+                 box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.8);
+             }
+            #universeimg1.img{
+              width:100%;
+              height:100%;
+            }
+            #solarimg1{
+              display:flex;
+              direction:row;
+              height:400px;
+              width:300px;
+            }
+            #solarimg5{
+              height:400px;
+              width:400px;
+            }
+        #universeimg2{
+             display:flex;
+               align-content:center;
+               justify-content:stretch;
+                 background-repeat:no-repeat;
+                               width:280px;
+                                           height:280px;
+                                margin:5px;
+                                overflow:hidden;
+                 float:right;
+                 border-radius:20px;
+                 box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.8);
+             }
+              #universeimg2.img{
+              width:100%;
+              height:100%;
+            }
+            @media screen and (max-width:820px){
+               .text1{ 
+             font-size:50px;
+                   text-align:center;
+                                }
+                        }
+                                    h1{
+                
+                 height:50px;
+                 margin:0px;
+             background:linear-gradient(90deg,#0D1282,#000000);
+             text-align:center;
+                 font-family:serif;
+                 color:#EEEDED;
+             }
+             h2{
+                 height:50px;
+             background:linear-gradient(90deg,#0D1282,#000000);
+             text-align:left;
+                 font-family:serif;
+                 color:#EEEDED;
+                 padding:10px;
+             }
+             #SolarSystem{
+              display:none;
+             }
+             .solartext{
+                  background:linear-gradient(90deg,#0D1282,#000000);
+                 text-indent:15px;
+             font-size:20px;
+                 color:#EEEDED;
+                 font-family:arial;
+             }
+                      .starh1{
+                
+                 height:50px;
+                 margin:0px;
+             background:linear-gradient(90deg,#000000,#7A0BC0);
+             text-align:center;
+                 font-family:serif;
+                 color:#EA1179;
+             }
+                  .startext{
+                  background:linear-gradient(90deg,#000000,#7A0BC0);
+                 text-indent:15px;
+             font-size:20px;
+                 color:#FE7BE5;
+                 font-family:arial;
+             }
+            #starimg5{
+               width:400px;
+             }
+             #starimg2{
+               height:450px;
+               width:300px;
+             }
+             #starimg3{
+               width:400px;
+               height:500px;
+                            }
+                            #starimg4{
+                              width:380px;
+                            }
+             #Stars{
+                       display:none;
+                    }
+                        .left{
+             display:flex;
+               align-content:center;
+               justify-content:center;
+                 background-repeat:no-repeat;
+                               width: 450px;
+                                           height: 300px;
+                                margin:5px;
+                                overflow:hidden;
+                 float:left;
+                 border-radius:20px;
+                 box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.8);
+             }
+              .right{
+             display:flex;
+               align-content:center;
+               justify-content:center;
+                 background-repeat:no-repeat;
+                               width: 450px;
+                                           height: 300px;
+                                margin:5px;
+                                overflow:hidden;
+                 float:right;
+                 border-radius:20px;
+                 box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.8);
+             }
+               .right > img{
+             width: 100%;
+             }
+             .left>img{
+             width:100%;
+             }
+                   .asteroidh1{
+                  height:50px;
+                 margin:0px;
+             background:linear-gradient(90deg,#071952,#1B6B93);
+             text-align:center;
+                 font-family:serif;
+                 color:#DFD7BF;
+             }
+             .asteroidh2{
+                 height:50px;
+             background:linear-gradient(90deg,#071952,#1B6B93);
+             text-align:left;
+                 font-family:serif;
+                 color:#DFD7BF;
+                 padding:10px;
+             }
+             .asteroidtext{
+                  background:linear-gradient(90deg,#071952,#1B6B93);
+                 text-indent:15px;
+             font-size:20px;
+                 color:#DFD7BF;
+                 font-family:arial;
+             }
+ #asteroidimg3{
+                 width:300px;
+             height:300px;
+             }
+              #asteroidimg6{
+                 width:360px;
+             }
+             #asteroids{
+               display:none;
+                            }
+                            .meteorh1{
+                
+                 height:50px;
+                 margin:0px;
+             background:linear-gradient(90deg,#9b59b6,#191D88);
+             text-align:center;
+                 font-family:serif;
+                 color:#DFD7BF;
+             }
+             .meteorh2{
+                 height:50px;
+             background:linear-gradient(90deg,#9b59b6,#191D88);
+             text-align:left;
+                 font-family:serif;
+                 color:#DFD7BF;
+                 padding:10px;
+             }
+             .meteortext{
+                  background:linear-gradient(90deg,#9b59b6,#191D88);
+                 text-indent:15px;
+             font-size:20px;
+                 color:#DFD7BF;
+                 font-family:arial;
+             }
+                    #meteors{
+         display:none;
+       }
+        .cometh1{
+                
+                 height:50px;
+                 margin:0px;
+             background:linear-gradient(90deg,#C400C6,#1450A3);
+             text-align:center;
+                 font-family:serif;
+                 color:#F1F0E8;
+             }
+             .cometh2{
+                 height:50px;
+             background:linear-gradient(90deg,#C400C6,#1450A3);
+             text-align:left;
+                 font-family:serif;
+                 color:#F1F0E8;
+                 padding:10px;
+             }
+             .comettext{
+                  background:linear-gradient(90deg,#C400C6,#1450A3);
+                 text-indent:15px;
+             font-size:20px;
+                 color:#F1F0E8;
+                 font-family:arial;
+             }
+             #comets{
+               display:none;
+             }
